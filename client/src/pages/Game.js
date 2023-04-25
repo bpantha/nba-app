79e3d8f49321b418d31123611a90b509f2d53eaa
Suniul Karim <102999067+sunnykarim@users.noreply.github.com>
2023-04-24 17:04:06 -0700

    commit game
    dlkjd

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -5,39 +5,23 @@ import { debounce } from 'lodash';
 const config = require("../config.json");
 
 const Game = () => {
-  const [games, setGames] = useState([]);
   const [seasons, setSeasons] = useState([2015]);
   const [topUpsets, setTopUpsets] = useState([]);
 
   const handleSeasonsChange = useCallback(
     debounce((newSelectedSeason) => {
       setSeasons(newSelectedSeason);
-  
-      if (newSelectedSeason <= 2015) {
-        const fetchData = async () => {
-          const res = await fetch(
-            `http://${config.server_host}:${config.server_port}/upsets/${newSelectedSeason}`
-          );
-          const resJson = res ? await res.json() : null;
-          setTopUpsets(resJson || []);
-        };
-        fetchData();
-      } else {
-        setTopUpsets([]);
-      }
     }, 50),
-    [seasons]
+    [] // Remove seasons from the dependencies array
   );
 
   useEffect(() => {
-    const queryParams = new URLSearchParams({
-      seasons,
-    }).toString();
-
     const fetchData = async () => {
-      fetch(`http://${config.server_host}:${config.server_port}/games?${queryParams}`)
+      fetch(
+        `http://${config.server_host}:${config.server_port}/upsets/${seasons}`
+      )
         .then((res) => res.json())
-        .then((resJson) => setGames(resJson));
+        .then((resJson) => setTopUpsets(resJson));
     };
     fetchData();
   }, [seasons]);
@@ -56,9 +40,6 @@ const Game = () => {
       <SeasonSelect onSeasonsChange={handleSeasonsChange} value={seasons} setValue={setSeasons} />
       <h1 style={h1Style}>Top Upsets</h1>
       <LazyTable data={topUpsets} seasons={seasons} />
-  
-      <h1 style={h1Style}>Games</h1>
-      <LazyTable data={games} />
     </div>
   );
 };
