707971d2a50b5ff24acb857991ffbfdf50d21cc0
Suniul Karim <102999067+sunnykarim@users.noreply.github.com>
2023-04-24 19:52:46 -0700

    WIP
    hi

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,12 +1,13 @@
 import { useState, useCallback, useEffect } from "react";
-import { SeasonSelect } from '../components/SeasonSelect';
-import { LazyTable } from '../components/LazyTable';
-import { debounce } from 'lodash';
+import { SeasonSelect } from "../components/SeasonSelect";
+import { LazyTable } from "../components/LazyTable";
+import { debounce } from "lodash";
 const config = require("../config.json");
 
 const Game = () => {
   const [seasons, setSeasons] = useState([2015]);
   const [topUpsets, setTopUpsets] = useState([]);
+  const [awardByTeam, setAwardByTeam] = useState([]);
 
   const handleSeasonsChange = useCallback(
     debounce((newSelectedSeason) => {
@@ -26,20 +27,36 @@ const Game = () => {
     fetchData();
   }, [seasons]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `http://${config.server_host}:${config.server_port}/awards_by_team/`
+      )
+        .then((res) => res.json())
+        .then((resJson) => setAwardByTeam(resJson));
+    };
+    fetchData();
+  }, []);
+
   const h1Style = {
     marginRight: "30px",
     fontFamily: "monospace",
     fontWeight: 700,
     letterSpacing: ".3rem",
-    textAlign: 'center',
-    color: '#2E4A62'
+    textAlign: "center",
+    color: "#2E4A62",
   };
 
   return (
     <div>
-      <SeasonSelect onSeasonsChange={handleSeasonsChange} value={seasons} setValue={setSeasons} />
+      <SeasonSelect
+        onSeasonsChange={handleSeasonsChange}
+        value={seasons}
+        setValue={setSeasons}
+      />
       <h1 style={h1Style}>Top Upsets</h1>
       <LazyTable data={topUpsets} seasons={seasons} />
+      <LazyTable data={awardByTeam} seasons={seasons} />
     </div>
   );
 };
